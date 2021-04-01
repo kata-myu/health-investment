3f004d5d5ea30785a1031d62477b93b9dfd19e1b
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-04-01 14:28:33 +0900

    保存機能

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,15 @@
 class PlansController < ApplicationController
   def index
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.create(plan_params)
+    redirect_to action: :index
+  end
+
+  private
+  def plan_params
+    params.require(:plan).permit(:plan, :date).merge(user_id: current_user.id)
   end
 end
