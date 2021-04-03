a732b09f10138f0fdbec6ccec7e6701ca76092a1
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-04-03 17:29:25 +0900

    一覧表示機能

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,7 @@
 class PlansController < ApplicationController
   def index
+    get_week
+    @month = Time.current.all_month
     @plan = Plan.new
   end
 
@@ -12,4 +14,29 @@ class PlansController < ApplicationController
   def plan_params
     params.require(:plan).permit(:plan, :date).merge(user_id: current_user.id)
   end
+
+  def get_week
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+    @todays_date = Date.today
+
+    @week_days = []
+
+    @plans = Plan.where(date: @todays_date..@todays_date + 30)
+
+    30.times do |x|
+      plans = []
+      plan = @plans.map do |plan|
+        plans.push(plan.plan) if plan.date == @todays_date + x   #←1回目は@todays_date + xに合うものしかplansにpushされない。4/1の予定が「サッカー」だけなら["サッカー"]となり、「サッカー」と「買い物」なら["サッカー", "買い物"]となる。
+      end
+
+      wday_num = (@todays_date + x).wday
+
+      
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, wday: wdays[wday_num], :plans => plans}
+      @week_days.push(days)
+    end
+  end
+  
 end
