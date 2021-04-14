9455bea877f8c8b2eb461e357f577c1d700780a3
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-04-14 23:23:26 +0900

    usersテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2021_04_05_095023) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
+    t.string "goal", null: false
+    t.date "registration_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
