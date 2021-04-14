bf908436281d6c7792b42edaf6c0cfe7555e28bd
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-04-14 23:39:10 +0900

    htmlの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters , if: :devise_controller?
   before_action :configure_account_update_params, only: [:update], if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :point])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :point, :goal, :registration_date])
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :point])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :point, :goal, :registration_date])
   end
 end
