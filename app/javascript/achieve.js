46b69909a4a9b4439889afb63df01ca36b826c29
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-04-15 02:05:21 +0900

    プランの表示修正

--- app/javascript/achieve.js
+++ app/javascript/achieve.js
@@ -1,9 +1,19 @@
 const achieve = () => {
   const checkboxes = document.querySelectorAll(".achieve-check");
   checkboxes.forEach((checkbox) => {
+    if (checkbox.getAttribute("data-load") != null) {
+      return null;
+    }
+    checkbox.setAttribute("data-load", "true");
+
     checkbox.addEventListener("click", (e) => {
       const id = e.target.getAttribute("data-id");
-      console.log(id);
+
+      const checkBox = e.toElement; //発火した要素を取得
+      const parent = checkBox.parentNode;
+      const deleteBtn = parent.lastElementChild.lastElementChild;
+      console.log(deleteBtn);
+
       const XHR = new XMLHttpRequest();
       XHR.open("POST", `/achievements/?id=${id}`, true);
       XHR.responseType = "json";
@@ -12,6 +22,11 @@ const achieve = () => {
         const achievement = XHR.response.achievement;
         console.log(achievement);
         const run = XHR.response.run;
+        
+        deleteBtn.setAttribute("style", "pointer-events: none;"); //すでにイベントリスナがセットされてしまっているので、クリックできなくする。
+        deleteBtn.innerHTML = "達成済み" 
+        checkBox.setAttribute("style", "pointer-events: none;");  //チェックボックスを押せなくする。
+
         if(run == true) {
           const done = document.getElementById("done");
           HTML = `<div>本日の予定は完了です！</div>`;
@@ -26,8 +41,19 @@ const achieve = () => {
 
   const checkeds = document.querySelectorAll(".achieve-checked");
   checkeds.forEach((checked) => {
+    if (checked.getAttribute("data-load") != null) {
+      return null;
+    }
+    checked.setAttribute("data-load", "true");
+
     checked.addEventListener("click", (e) => {
       const id = e.target.getAttribute("data-id");
+
+      const checkBox = e.toElement; //発火した要素を取得
+      const parent = checkBox.parentNode;
+      const deleteBtn = parent.lastElementChild.lastElementChild;
+      console.log(deleteBtn);
+
       console.log(id);
       const XHR = new XMLHttpRequest();
       XHR.open("DELETE", `/achievements/${id}`, true);
@@ -36,6 +62,10 @@ const achieve = () => {
       XHR.onload = () => {
         const achievement = XHR.response.achievement;
         console.log(achievement);
+
+        deleteBtn.removeAttribute("style"); 
+        deleteBtn.innerHTML = "削除";
+        checkBox.setAttribute("style", "pointer-events: none;");
       };
       XHR.onerror = () => {
         alert("リクエストに失敗しました");
@@ -43,4 +73,4 @@ const achieve = () => {
     });
   });
 };
-window.addEventListener("load", achieve);
+setInterval(achieve, 1000);
