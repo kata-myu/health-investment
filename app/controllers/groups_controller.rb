f8242856e951cd4e1c6a17c143945e09c9f07221
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-04-19 06:43:05 +0900

    グループページの修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,7 +13,7 @@ class GroupsController < ApplicationController
     if @group.valid?
       @group.save
       GroupUser.create(group_id: @group.id, user_id: current_user.id)
-      redirect_to root_path
+      redirect_to groups_path
     else
       render :new
     end
@@ -28,6 +28,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, :promotion)
   end
 end
