71708fd63b5fa37637d2fb4b1a3a7d936d136fbc
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-04-18 14:46:59 +0900

    紹介（宣伝）カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_04_17_163103) do
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
+    t.string "promotion", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
