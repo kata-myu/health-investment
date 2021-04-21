4b7cf25c22195fa99899f96d1953def5ebbc12c9
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-04-22 04:33:37 +0900

    notification変更

--- app/javascript/notification.js
+++ app/javascript/notification.js
@@ -1,5 +1,34 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-  Notification.requestPermission(permission=>{
-    const notification = new Notification("Check!");
-  });
-});
+const notificationAction = () =>{
+  // function checkVersion() {
+  //   const latestVersion = document.querySelector(".js-currentVersion").textContent;
+  //   const currentVersion = localStorage.getItem("conciseVersion");
+  
+  //   if (currentVersion === null || semverCompare(currentVersion, latestVersion) === -1) {      
+  //     const notification = new Notification("Hello, world!");
+  
+  //     localStorage.setItem("conciseVersion", latestVersion);
+  //   }
+  // }
+
+  if (!('Notification' in window)) {
+    alert('未対応のブラウザです');
+  }
+  else {
+    // 許可を求める
+    Notification.requestPermission()
+      .then((permission) => {
+        if (permission == 'granted') {
+          // 許可
+          const n = Notification("リマインドです");
+          
+        } else if (permission == 'denied') {
+          // 拒否
+          return;
+        } else if (permission == 'default') {
+          // 無視
+          return;
+        }
+      });
+  }
+}
+window.addEventListener("load", notificationAction)
