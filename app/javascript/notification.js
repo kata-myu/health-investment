90484a09d0c7ecca2a4d91d769b755b74a3c54a1
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-04-22 04:27:17 +0900

    通知機能

--- app/javascript/notification.js
+++ app/javascript/notification.js
@@ -1,38 +1,5 @@
-// const point = () => {
-//   const XHR = new XMLHttpRequest();
-//   XHR.open("GET", "", true);
-//   XHR.responseType = "json";
-//   XHR.send();
-// }
-
-
-// const notification = () => {
-//   const payload = {
-//     text: "ポイントが付与される日です！\n" +
-//           "サイドバーから確認できます。" + "\n" +
-//           "継続することで、健康投資効果がどんどん高まります！" + "\n"
-//   }
-
-//   const url = ""
-
-//   fetch(url, {
-//     method: 'POST',
-//     body: JSON.stringify(payload)
-//   }).then(() => {
-//     alert('送信が完了しました。追ってご連絡します！')
-//   })
-// };
-
-// const checkTime = () => {
-//   let preDate; 
-//   setInterval(checkTime, 100000)
-//   const currentTime = new Date();
-//   const date = currentTime.getDate();
-//   console.log(date);
-//   if (preDate !== date && date % 5 === 0) {
-//     preDate = date;
-//     point
-//     notification
-//   }
-// };
-// window.addEventListener("load", checkTime);
+window.addEventListener('DOMContentLoaded', ()=>{
+  Notification.requestPermission(permission=>{
+    const notification = new Notification("Check!");
+  });
+});
