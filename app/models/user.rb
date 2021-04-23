05b34cb2e1155ffe24a7d1763d348539a9c0de98
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-04-23 16:56:30 +0900

    エラーメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 20 }
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "Include both letters and numbers"}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "は半角英数字を含む６文字以上"}
 
   has_many :group_users, dependent: :destroy
   has_many :groups, through: :group_users
