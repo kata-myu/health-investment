f4266733e28f3fbd42cbbdb77bfc95aa1f3c9515
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-06-28 19:42:20 +0900

    ポイント機能一時的に停止

--- app/javascript/point.js
+++ app/javascript/point.js
@@ -1,62 +1,62 @@
-const point = () => {
-  const XHR = new XMLHttpRequest();
-  XHR.open("GET", " /plans/point", true);
-  XHR.responseType = "json";
-  XHR.send();
-  XHR.onload = () => {
-    const point = XHR.response.point;
-    console.log(point);
-    if(point == "already"){
-      return null;
-    }else{
-    const pointMessage = `<p style="color: white;">${point}pt付与されました！</p>`;
-    const messageArea = document.getElementById("point-message");
-    messageArea.insertAdjacentHTML("beforeend", pointMessage);
-    }
+// const point = () => {
+//   const XHR = new XMLHttpRequest();
+//   XHR.open("GET", " /plans/point", true);
+//   XHR.responseType = "json";
+//   XHR.send();
+//   XHR.onload = () => {
+//     const point = XHR.response.point;
+//     console.log(point);
+//     if(point == "already"){
+//       return null;
+//     }else{
+//     const pointMessage = `<p style="color: white;">${point}pt付与されました！</p>`;
+//     const messageArea = document.getElementById("point-message");
+//     messageArea.insertAdjacentHTML("beforeend", pointMessage);
+//     }
 
-    if (XHR.status != 200) {
-      alert (`Error ${XHR.status}: ${XHR.statusText}`);
-    } else {
-      return null;
-    }
-  };
-};
+//     if (XHR.status != 200) {
+//       alert (`Error ${XHR.status}: ${XHR.statusText}`);
+//     } else {
+//       return null;
+//     }
+//   };
+// };
 
 
-const notification = () => {
-  const payload = {
-    text: "ポイントが付与される日です！\n" +
-          "サイドバーで確認しましょう。" + "\n" +
-          "継続することで、健康投資効果がどんどん高まりますね！" + "\n"
-  }
+// const notification = () => {
+//   const payload = {
+//     text: "ポイントが付与される日です！\n" +
+//           "サイドバーで確認しましょう。" + "\n" +
+//           "継続することで、健康投資効果がどんどん高まりますね！" + "\n"
+//   }
 
-  const url = ""
+//   const url = ""
 
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(payload)
-  }).then(() => {
-    console.log('ポイントが付与されました！');
-  })
-};
+//   fetch(url, {
+//     method: 'POST',
+//     body: JSON.stringify(payload)
+//   }).then(() => {
+//     console.log('ポイントが付与されました！');
+//   })
+// };
 
-const checkTime = () => {
-  let preDate; 
-  const pointTime = () => {
-    const currentTime = new Date();
-    const date = currentTime.getDate();
+// const checkTime = () => {
+//   let preDate; 
+//   const pointTime = () => {
+//     const currentTime = new Date();
+//     const date = currentTime.getDate();
     
-    if (preDate !== date && date % 5 === 0) {
-      preDate = date;
-      point()
-      notification()
-    }
-  };
+//     if (preDate !== date && date % 5 === 0) {
+//       preDate = date;
+//       point()
+//       notification()
+//     }
+//   };
 
-  setInterval(pointTime, 10000);
-};
+//   setInterval(pointTime, 10000);
+// };
 
-window.addEventListener("load", checkTime);
+// window.addEventListener("load", checkTime);
 
 
 
